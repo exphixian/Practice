86094ee956f9100515f69f06f92717fec0b06db1
Kathleen Wilson <37256276+exphixian@users.noreply.github.com>
2021-09-06 19:32:15 -0700

    Create stop.py

---
+++ stop.py
@@ -0,0 +1,9 @@
+### Randomness inspired by XKCD #210
+import random
+
+print("STOP")
+number = random.randint(1,2)
+if number == 1:
+    print("Hammertime!")
+else:
+    print("collaborate and listen!")
